a9d2cdc67ced6300f85ba9c07c0460c01da00d97
vidumini-atha <viduathapaththu@gmail.com>
2025-04-04 15:34:40 +0530

    test pipeline 5

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -55,7 +55,7 @@ const Header = () => {
         <div className="navigation d-flex align-items-center justify-content-between">
           <div className="logo">
             <h2 className="d-flex align-items-center gap-1">
-              <i className="custom-icon"></i> courseHub
+              <i className="custom-icon"></i> CourseHUB
             </h2>
           </div>
 
